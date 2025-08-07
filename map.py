f66ce84d50cfc91f290c9c2603c029196eb2fb01
rashik24 <rsiddiq2@ncsu.edu>
2025-08-07 07:09:00 -0400

    Update map.py

--- map.py
+++ map.py
@@ -13,7 +13,7 @@ import os
 # Set your Mapbox token as environment variable
 os.environ["MAPBOX_API_KEY"] = "pk.eyJ1IjoicnNpZGRpcTIiLCJhIjoiY21jbjcwNWtkMHV5bzJpb2pnM3QxaDFtMyJ9.6T6i_QFuKQatpGaCFUvCKg"
 
-ODM_CSV = "ODM FBCENC 2.csv"
+ODM_CSV = "ODM_CAFN.csv"
 TRACTS_SHP = "cb_2023_37_tract_500k.shp"
 OPENCAGE_API_KEY = "f53bdda785074d5499b7a4d29d5acd1f"
 geocoder = OpenCageGeocode(OPENCAGE_API_KEY)
@@ -79,7 +79,7 @@ if user_address:
         agencies_nearby["total_traveltime"] = agencies_nearby["total_traveltime"].round(2)
         agencies_nearby["total_miles"] = agencies_nearby["total_miles"].round(2)
 
-        display_cols = ["agency_name", "address", "city", "total_traveltime", "total_miles"]
+        display_cols = ["Agency Name", "Address", "total_traveltime", "total_miles"]
         st.dataframe(agencies_nearby[display_cols].drop_duplicates().sort_values("total_traveltime"))
 
         # ─── MAP ────────────────────────────────────────────────────────
